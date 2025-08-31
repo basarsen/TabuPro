cb317b6b1815bea270ebdb20a1b7703be155b477
basarsen@hotmail.com <basarsen@hotmail.com>
2025-08-31 07:36:07 +0300

    initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import styles from './Button.module.scss'
+
+export default function Button() {
+    return (
+        <div className={styles.container}>Button</div>
+    )
+}
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Button from "./Button/Button";
+import Radio from "./Radio/Radio";
+
+export {
+    Button,
+    Radio
+}
